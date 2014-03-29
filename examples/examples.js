1e93d8891d22ce00d5d357e16ee636659c7abd2c
Gustav Nikolaj Olsen <gno@one.com>
2014-03-29 22:12:14 +0100

    Adding another example

--- examples/examples.js
+++ examples/examples.js
@@ -1,15 +1,19 @@
 function ViewModel() {
+    var that = this;
+
     this.example1 = ko.observableArray([2,1,2,3]);
+    this.example1Add = function () {
+        var min = 0,
+            max = 9,
+            randomNumber = Math.random() * (max - min) + min;
+
+        that.example1.push(randomNumber);
+    };
+
+    this.example2 = ko.computed(function () {
+        return that.example1().slice(-10);
+    });
 }
-
-ViewModel.prototype.example1Add = function () {
-    var min = 0,
-        max = 9,
-        randomNumber = Math.random() * (max - min) + min;
-
-    this.example1.push(randomNumber);
-};
-
 
 var viewModel = new ViewModel();
 
