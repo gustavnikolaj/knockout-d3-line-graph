317636e2a18ef3fe83913dabaf43ed8fafd5eb1b
Gustav Nikolaj Olsen <gno@one.com>
2014-03-29 22:21:38 +0100

    Add pages

---
+++ examples.js
@@ -0,0 +1,20 @@
+function ViewModel() {
+    var that = this;
+
+    this.example1 = ko.observableArray([2,1,2,3]);
+    this.example1Add = function () {
+        var min = 0,
+            max = 9,
+            randomNumber = Math.random() * (max - min) + min;
+
+        that.example1.push(randomNumber);
+    };
+
+    this.example2 = ko.computed(function () {
+        return that.example1().slice(-10);
+    });
+}
+
+var viewModel = new ViewModel();
+
+ko.applyBindings(viewModel);
---
+++ knockout-d3-line-graph.js
@@ -0,0 +1,71 @@
+/*global ko, d3*/
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory(require('knockout'), require('d3'));
+    } else if (typeof define === 'function' && define.amd) {
+        define(['knockout', 'd3'], factory);
+    } else {
+        factory(ko, d3);
+    }
+}(this, function (ko, d3) {
+
+    function getPaintingMethods(data, element) {
+        var elementRect = element.getBoundingClientRect(),
+            width = elementRect.width,
+            height = elementRect.height,
+            x = d3.scale.linear().domain([0, data.length-1]).range([0, width]),
+            y = d3.scale.linear().domain([-1, d3.max(data)+1]).range([height, 0]);
+
+        return {
+            line: d3.svg.line().interpolate('basis')
+                .x(function(d,i) { return x(i); })
+                .y(function(d) { return y(d); }),
+            area: d3.svg.area().interpolate('basis')
+                .x(function(d,i) { return x(i); })
+                .y0(height)
+                .y1(function(d) { return y(d); })
+        };
+    }
+
+
+    ko.bindingHandlers.d3LineGraph = {
+        init: function (element, valueAccessor) {
+            var bindingContext = ko.utils.unwrapObservable(valueAccessor());
+            var elementRect = element.getBoundingClientRect();
+            var data = bindingContext;
+            var shapes = getPaintingMethods(data, element);
+
+            var svg = d3.select(element).append('svg');
+
+            svg.attr('width', elementRect.width)
+               .attr('height', elementRect.height);
+
+            svg.append('path').attr('class', 'area').attr('d', shapes.area(data));
+            svg.append('path').attr('class', 'path').attr('d', shapes.line(data));
+        },
+        update: function (element, valueAccessor) {
+            var bindingContext = ko.utils.unwrapObservable(valueAccessor());
+            var data = bindingContext;
+            var shapes = getPaintingMethods(data, element);
+
+            var svg = d3.select(element).select('svg');
+
+            svg.select('path.area')
+               .interrupt()
+               .transition()
+               .ease('linear')
+               .duration(250)
+               .attr('d', shapes.area(data));
+
+            svg.select('path.path')
+               .interrupt()
+               .transition()
+               .ease('linear')
+               .duration(250)
+               .attr('d', shapes.line(data));
+        }
+    };
+
+    return ko.bindingHandlers.d3LineGraph;
+
+}));
