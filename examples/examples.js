7185699f166ae1aa0dd2908bcf158172d94262d1
Sander van Vliet <sauron@barad-dur.nl>
2015-03-13 16:34:23 +0100

    Added example for a complex datatype binding

--- examples/examples.js
+++ examples/examples.js
@@ -9,11 +9,22 @@ function ViewModel() {
             randomNumber = Math.random() * (max - min) + min;
 
         that.example1.push(randomNumber);
+        that.complexData.push({ position: new Date(2015, 1, that.complexData().length + 1), value: randomNumber });
     };
 
     this.example2 = ko.computed(function () {
         return that.example1().slice(-10);
     });
+
+    this.complexData = ko.observableArray([
+        { position: new Date(2015, 1, 1), value: 10 },
+        { position: new Date(2015, 1, 2), value: 8 },
+        { position: new Date(2015, 1, 3), value: 12 },
+        { position: new Date(2015, 1, 4), value: 13 },
+        { position: new Date(2015, 1, 5), value: 11 },
+        { position: new Date(2015, 1, 6), value: 7 },
+        { position: new Date(2015, 1, 7), value: 5 }
+    ]);
 }
 
 var viewModel = new ViewModel();
